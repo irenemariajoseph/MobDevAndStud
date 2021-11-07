c8812621a866ca49f63884d9d944839b646935fc
leonyhhana24 <leony.zebua@student.umn.ac.id>
2021-11-07 20:14:50 +0700

    API Google + Update Detail Button

---
+++ app/src/main/java/com/example/mobappproject/GeoLocation.java
@@ -0,0 +1,54 @@
+package com.example.mobappproject;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.os.Message;
+import android.webkit.GeolocationPermissions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.logging.Handler;
+
+public class GeoLocation extends GeolocationPermissions {
+    public static void getAddress(final String locationAddress, final Context context, Handler handler) {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+                String result = null;
+                try {
+                    List addressList = geocoder.getFromLocationName(locationAddress, 1);
+                    if (addressList != null && addressList.size() > 0) {
+                        Address address = (Address) addressList.get(0);
+                        StringBuilder stringBuilder = new StringBuilder();
+                        stringBuilder.append(address.getLatitude()).append("\n");
+                        stringBuilder.append(address.getLongitude()).append("\n");
+                        result = stringBuilder.toString();
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    Message message = Message.obtain();
+                    message.setTarget(handler);
+                    if (result != null) {
+                        message.what = 1;
+                        Bundle bundle = new Bundle();
+                        result = "Address      : " + locationAddress +
+                                "\n\n\nLatitude and Longitude\n" + result;
+                        bundle.putString("address", result);
+                        message.setData(bundle);
+                    }
+                    message.sendToTarget();
+
+                }
+
+            }
+        };
+        thread.start();
+    }
+}
+
